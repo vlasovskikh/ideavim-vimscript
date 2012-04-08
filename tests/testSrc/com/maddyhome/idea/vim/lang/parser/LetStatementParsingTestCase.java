ba032adece157693e940157f7af86613b2b801f2
Ksenia V. Mamich <kvmamich@gmail.com>
2012-04-08 04:37:28 +0400

    Added tests for 'let' stmt.

---
+++ tests/testSrc/com/maddyhome/idea/vim/lang/parser/LetStatementParsingTestCase.java
@@ -0,0 +1,22 @@
+package com.maddyhome.idea.vim.lang.parser;
+
+/**
+ * <p>Date: 08.04.12</p>
+ *
+ * @author Ksenia V. Mamich
+ * @version 1.0
+ */
+public class LetStatementParsingTestCase extends VimScriptParsingTestCase {
+  public LetStatementParsingTestCase() {
+    super("parser/let");
+  }
+
+  public void testLet0() { doParserTest("let"); }
+  public void testLet1() { doParserTest("let\n"); }
+
+  //public void testLetError0() { doParserTest("let var="); }
+
+  public void testLetVar0() { doParserTest("let var"); }
+
+
+}
