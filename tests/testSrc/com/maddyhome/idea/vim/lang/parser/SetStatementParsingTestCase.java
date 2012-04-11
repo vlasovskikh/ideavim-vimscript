a555de49404465ec0829f81552ef8686756ea31e
Ksenia V. Mamich <kvmamich@gmail.com>
2012-04-12 01:17:36 +0400

    New test for 'set' statement.

--- tests/testSrc/com/maddyhome/idea/vim/lang/parser/SetStatementParsingTestCase.java
+++ tests/testSrc/com/maddyhome/idea/vim/lang/parser/SetStatementParsingTestCase.java
@@ -16,10 +16,34 @@ public class SetStatementParsingTestCase extends VimScriptParsingTestCase {
   public void testSet2() { doParserTest("set\n"); }
   public void testSet3() { doParserTest("se\n"); }
 
-  public void testSetAll0() { doParserTest("set all"); }
-  public void testSetAll1() { doParserTest("set all&"); }
+  public void testSetAll() { doParserTest("set all"); }
+  public void testSetAllAmp() { doParserTest("set all&"); }
 
   public void testSetTermcap() { doParserTest("set termcap"); }
 
+  public void testOption() { doParserTest("set opt"); }
+  public void testOptionQuestion() { doParserTest("set opt?"); }
+  public void testOptionAmp() { doParserTest("set opt&"); }
+
+  /*
+  public void testOptionAmpVi() { doParserTest("set opt&vi"); }
+  public void testOptionAmpVim() { doParserTest("set opt&vim"); }
+  */
+
+  public void testOptionReset() { doParserTest("set noopt"); }
+
+  public void testOptionInv0() { doParserTest("set invopt"); }
+  public void testOptionInv1() { doParserTest("set opt!"); }
+
+  public void testOptionAssignment0() { doParserTest("set opt=val"); }
+  public void testOptionAssignment1() { doParserTest("set opt =val"); }
+  public void testOptionAssignment2() { doParserTest("set opt=3"); }
+  public void testOptionAssignment3() { doParserTest("set opt =3"); }
+  public void testOptionAssignment4() { doParserTest("set opt:0x13"); }
+  public void testOptionAssignment5() { doParserTest("set opt :0x13"); }
+
+  public void testOptionAssignment6() { doParserTest("se opt +=val"); }
+  public void testOptionAssignment7() { doParserTest("se opt -=val"); }
+  public void testOptionAssignment8() { doParserTest("se opt ^=val"); }
 
 }
